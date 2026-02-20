7d859272097f4677b43669bb74092fa2ea693889
LovePathak4 <lokeshpathak378@gmail.com>
2026-02-20 23:38:38 +0530

    sudoku solver

---
+++ Backtrack.java
@@ -0,0 +1,96 @@
+class Backtrack{
+
+    public static boolean isSafe(int Sudoku[][], int row , int col, int digit){
+        // column
+        for(int i=0; i<=8; i++){
+            if(Sudoku[i][col]==digit){
+                return false;
+            }
+        }
+        //row
+        for(int j=0; j<=8; j++){
+            if(Sudoku[row][j]==digit){
+                return false;
+            }
+        }
+        // grid
+        int sr=(row/3)*3;
+        int sc=(col/3)*3;
+        
+        //3*3 grid
+
+        for(int i=sr; i<sr+3; i++){
+            for(int j=sc; j<sc+3; j++){
+                if(Sudoku[i][j]==digit){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean SudokuSolver(int Sudoku[][], int row , int col){
+        // base case 
+        if(row==9 &&col==0){
+            return true;
+
+        }
+        // recursive
+        int nextRow=row;
+        int nextCol=col+1;
+        if(col+1==9){
+            nextRow=row+1;
+            nextCol=0;
+        }
+        if(Sudoku[row][col]!=0){
+            return SudokuSolver(Sudoku, nextRow, nextCol);
+        }
+        for(int digit=1; digit<=9; digit++){
+            if(isSafe(Sudoku, row, col, digit)){
+                Sudoku[row][col]=digit;
+                if(SudokuSolver(Sudoku, nextRow, nextCol)){
+                    return true;
+                }
+                Sudoku[row][col]=0;
+            }
+        }
+        return false;
+    }
+
+    public static void printSudoku(int Sudoku[][]){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                System.out.print(Sudoku[i][j]+" ");
+            }
+            System.out.println();   
+        }
+    }
+    
+    
+    
+    public static void main(String[] args) {
+     //   System.out.println("Hello World");
+
+     int Sudoku[][]={
+                     {0,7,8,3,2,0,0,0,5},
+                     {0,1,2,0,0,0,0,0,0},
+                     {6,0,0,0,8,7,0,0,2},
+                     {1,0,0,0,3,0,5,0,0},
+                     {0,5,0,0,0,0,0,0,3},
+                     {0,0,3,7,6,0,8,0,9},
+                     {0,3,0,8,0,0,0,0,1},
+                     {0,0,0,0,0,0,6,0,0},
+                     {0,0,7,0,0,9,0,5,0}
+     };
+         
+        if(SudokuSolver(Sudoku, 0, 0)){
+            System.out.println("solution exists");
+        }
+        else{
+            System.out.println("solution does not exist");
+        }
+
+        printSudoku(Sudoku);
+    }
+}
